41ebc4c1e42178ffffff2969cff832e06cdea8f9
Hicham Maghzaz <h.maghzaz@gmail.com>
2025-03-09 11:23:58 +0000

    implementation of team apis

--- src/main/java/com/morocco/fanPass/controller/TeamController.java
+++ src/main/java/com/morocco/fanPass/controller/TeamController.java
@@ -1,4 +1,42 @@
 package com.morocco.fanPass.controller;
 
+
+import com.morocco.fanPass.dto.TeamDTO;
+import com.morocco.fanPass.entity.Team;
+import com.morocco.fanPass.service.TeamService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/team")
 public class TeamController {
+
+    private final TeamService teamService;
+
+    public TeamController(TeamService teamService){
+        this.teamService = teamService;
+    }
+
+    @GetMapping("/all")
+    public List<TeamDTO> getAllTeams(){
+        return teamService.getAllTeams();
+    }
+
+    @GetMapping("/{id}")
+    public TeamDTO getTeamById(@PathVariable Long id){
+        return teamService.getTeamById(id);
+    }
+
+    @PostMapping("/create")
+    public TeamDTO createTeam(@RequestBody Team team){
+        return teamService.createTeam(team);
+    }
+
+    @PostMapping("/delete/{id}")
+    public ResponseEntity<String> deleteTeamById(@PathVariable Long id){
+        return teamService.deleteTeamById(id);
+    }
+
 }
--- src/main/java/com/morocco/fanPass/dto/TeamDTO.java
+++ src/main/java/com/morocco/fanPass/dto/TeamDTO.java
@@ -1,4 +1,20 @@
 package com.morocco.fanPass.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class TeamDTO {
+
+    private String name;
+    private String country;
+    private String flagImageUrl;
+
 }
